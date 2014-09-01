7bf681deba9a7da6be0155e640ee6b549b4183c9
cmew3 <c.wheeler-booth@cantab.net>
2014-09-01 14:19:05 +0100

    added nested form for mipoint and addresses

--- app/controllers/midpoints_controller.rb
+++ app/controllers/midpoints_controller.rb
@@ -1,5 +1,19 @@
 class MidpointsController < ApplicationController
-	def new
-      @midpoint = Midpoint.new 
+	
+  def index
+    
+  end
+
+  def new
+      @midpoint = Midpoint.new
+      @midpoint.addresses =[Address.new, Address.new]
+  end
+
+  def create
+    @midpoint = Midpoint.new(params.require(:midpoint).permit(addresses_attributes: [:id, :full_address]))
+    @midpoint.save
+    puts '---------------------'
+    puts @midpoint.addresses.inspect
+    redirect_to '/'
   end
 end
