788526af2a6a3a80061cfbea362a596e0838cd94
Jeremy Fox <foxjerem@gmail.com>
2014-09-01 09:46:10 +0100

    First algorithm test

---
+++ spec/models/calculators/midpoint_calculator_spec.rb
@@ -0,0 +1,14 @@
+describe MidpointCalculator do
+	
+	context 'distance - two point case' do
+
+		it 'should return coordinates for the equidistant point given 2 other points' do
+			point_1 = { lat: 50, lng: 50 }
+			point_2 = { lat: 20, lng: 10 }
+			points = [point_1, point_2]
+			expect(MidpointCalculator.find_by(:distance, points)).to eq({ lat: 35, lng: 30 })
+		end
+
+	end
+
+end
