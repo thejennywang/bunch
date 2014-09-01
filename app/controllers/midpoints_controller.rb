b76b640dc7f0846219680b5fbb10c5ff380979fc
cmew3 <c.wheeler-booth@cantab.net>
2014-09-01 14:27:53 +0100

    ready to move to development

--- app/controllers/midpoints_controller.rb
+++ app/controllers/midpoints_controller.rb
@@ -12,8 +12,6 @@ class MidpointsController < ApplicationController
   def create
     @midpoint = Midpoint.new(params.require(:midpoint).permit(addresses_attributes: [:id, :full_address]))
     @midpoint.save
-    puts '---------------------'
-    puts @midpoint.addresses.inspect
     redirect_to '/'
   end
 end
