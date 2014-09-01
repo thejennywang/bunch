1542c3da3f84a97243332a92a11a69a07b4a3db1
Jeremy Fox <foxjerem@gmail.com>
2014-09-01 10:31:24 +0100

    Added first distance midpoint calculation

--- app/models/calculators/midpoint_calculator.rb
+++ app/models/calculators/midpoint_calculator.rb
@@ -1,6 +1,15 @@
 class MidpointCalculator
 
 	def self.find_by(metric, points)
-		
+		latitude = _average_of(:lat, points)
+		longitude = _average_of(:lng, points)
+		Coordinate.new(latitude, longitude)
 	end
+
+	private
+
+	def self._average_of(attribute, array)
+		array.map(&attribute).inject(&:+) / array.length
+	end
+
 end
--- spec/models/calculators/midpoint_calculator_spec.rb
+++ spec/models/calculators/midpoint_calculator_spec.rb
@@ -4,11 +4,17 @@ describe MidpointCalculator do
 	
 	context 'distance - two point case' do
 
+		let(:point_1)	{ double Coordinate, lat: 50, lng: 80 }
+		let(:point_2) { double Coordinate, lat: 20, lng: 10 }
+		let(:points)	{ [point_1, point_2] }
+
+		it 'should return a coordinate object' do
+			expect(MidpointCalculator.find_by(:distance, points)).to be_an_instance_of(Coordinate)
+		end
+
 		it 'should return coordinates for the equidistant point given 2 other points' do
-			point_1 = { lat: 50, lng: 50 }
-			point_2 = { lat: 20, lng: 10 }
-			points = [point_1, point_2]
-			expect(MidpointCalculator.find_by(:distance, points)).to eq({ lat: 35, lng: 30 })
+			expect(MidpointCalculator.find_by(:distance, points).lat).to eq 35
+			expect(MidpointCalculator.find_by(:distance, points).lng).to eq 45
 		end
 
 	end
