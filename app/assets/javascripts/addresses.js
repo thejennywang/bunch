75cca69e1dac830f5ba5a9423d0148d1481282e9
David Bayon <dbayon82@googlemail.com>
2014-09-02 12:20:19 +0100

    Refactored and separated Geocode javascript methods, created models js folder

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -1,34 +1,43 @@
-var northEast = new google.maps.LatLng(51.7422004, 0.310537);
-var southWest = new google.maps.LatLng(51.2415153, -0.5654233);
-londonBounds = new google.maps.LatLngBounds(southWest, northEast);
-
 $(document).ready(function() {
+  var northEast = new google.maps.LatLng(51.7422004, 0.310537);
+  var southWest = new google.maps.LatLng(51.2415153, -0.5654233);
+  londonBounds = new google.maps.LatLngBounds(southWest, northEast);
+
   $('.new_midpoint').on('submit', function(event) {
     event.preventDefault();
-    var path = this.href;
+    geocodedAddresses = [];
 
     $('.address').each(function(){
-      GMaps.geocode({
-        address: $(this).val(),
-        region: "UK",
-        bounds: londonBounds,
-        callback: function(results, status) {
-          if (status == 'OK') {
-            latlng = results[0].geometry.location;
-            if (!londonBounds.contains(latlng)) {
-              alert("Please enter an address in London")
-            }
-          }
-          else {
-            console.log("address error")
-          }
+      geocodeAddress($(this).val());
+    });
+
+    $.post(this.action, $(this).serialize(), function() {});
+  });
+
+});
+
+
+function geocodeAddress(addressString) {
+  GMaps.geocode({
+    address: addressString,
+    region: "UK",
+    bounds: londonBounds,
+    callback: function(results, status) {
+      if (status == 'OK') {
+        latlng = results[0].geometry.location;
+        if (londonBounds.contains(latlng)) {
+          populateAddressArray(results[0]);
+        } else {
+          alert("Please enter an address in London");
         }
-      })
-    })
+      }
+    }
+  });
+}
 
-    $.post(this.action, $(this).serialize(), function() {
-
-    })
-
-  })
-})
+function populateAddressArray(result) {
+  addressModel = new AddressModel();
+  addressModel.populate(result);
+  geocodedAddresses.push(addressModel);
+  console.log(geocodedAddresses);
+}
