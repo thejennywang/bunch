06829e816863409f97dc7d55caa32ede6cb3206c
David Bayon <dbayon82@googlemail.com>
2014-09-02 10:06:26 +0100

    Bounds working on Geocode search

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -1,18 +1,21 @@
+var northEast = new google.maps.LatLng(51.7422004, 0.310537);
+var southWest = new google.maps.LatLng(51.2415153, -0.5654233);
+londonBounds = new google.maps.LatLngBounds(southWest, northEast);
+
 $(document).ready(function() {
   $('.bunch-submit').on('click', function(event) {
     event.preventDefault();
-    $('.address').each(function(){
 
+    $('.address').each(function(){
       GMaps.geocode({
         address: $(this).val(),
         region: "UK",
+        bounds: londonBounds,
         callback: function(results, status) {
           if (status == 'OK') {
-            var londonResults = results.filter(function(result) {
-              return result['address_components'][2]['long_name'] === 'London';
-            })
-            console.log(londonResults)
+            console.log(results)
             var latlng = results[0].geometry.location;
+            console.log(latlng)
           }
           else {
             console.log("address error")
