3da545f10339d1a66efd8fca1da36f03954fca37
Jeremy Fox <foxjerem@gmail.com>
2014-09-03 10:55:45 +0100

    Added a map in results page

---
+++ app/assets/javascripts/midpoints.js
@@ -0,0 +1,31 @@
+$(document).ready( function () {
+	
+	if ( $('#main_map_holder').length ) {
+
+		mainMap = new GMaps ({
+			div: '#main_map',
+			lat: -12.043333,
+      lng: -77.028333
+		});
+
+		mainMap.addMarker ({
+			lat: -13.043333,
+      lng: -78.028333,
+      id: 1
+    });
+
+		mainMap.addMarker ({
+			lat: -14.043333,
+      lng: -79.028333,
+      id: 2
+    });
+
+		mainMap.addMarker ({
+			lat: -12.043333,
+      lng: -77.028333,
+      id: 3
+		});
+
+	};
+
+});
