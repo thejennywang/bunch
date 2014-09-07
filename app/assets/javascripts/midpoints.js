6fff4e8136c9608519c07029152208012fd24b43
David Bayon <dbayon82@googlemail.com>
2014-09-07 17:09:22 +0100

    Shifted center of map

--- app/assets/javascripts/midpoints.js
+++ app/assets/javascripts/midpoints.js
@@ -4,111 +4,117 @@ $(document).ready( function () {
 
 		var styleArray = [
 	    {
-	        "featureType": "water",
-	        "stylers": [
-	            {
-	                "visibility": "on"
-	            },
-	            {
-	                "color": "#acbcc9"
-	            }
-	        ]
+	      "featureType": "water",
+	      "stylers": [
+	        {
+	          "visibility": "on"
+	        },
+	        {
+	          "color": "#acbcc9"
+	        }
+	      ]
 	    },
 	    {
-	        "featureType": "landscape",
-	        "stylers": [
-	            {
-	                "color": "#f2e5d4"
-	            }
-	        ]
+	      "featureType": "landscape",
+	      "stylers": [
+	        {
+	          "color": "#f2e5d4"
+	        }
+	      ]
 	    },
 	    {
-	        "featureType": "road.highway",
-	        "elementType": "geometry",
-	        "stylers": [
-	            {
-	                "color": "#c5c6c6"
-	            }
-	        ]
+	      "featureType": "road.highway",
+	      "elementType": "geometry",
+	      "stylers": [
+          {
+            "color": "#c5c6c6"
+          }
+        ]
 	    },
 	    {
-	        "featureType": "road.arterial",
-	        "elementType": "geometry",
-	        "stylers": [
-	            {
-	                "color": "#e4d7c6"
-	            }
-	        ]
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e4d7c6"
+          }
+        ]
 	    },
 	    {
-	        "featureType": "road.local",
-	        "elementType": "geometry",
-	        "stylers": [
-	            {
-	                "color": "#fbfaf7"
-	            }
-	        ]
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#fbfaf7"
+          }
+        ]
 	    },
 	    {
-	        "featureType": "poi.park",
-	        "elementType": "geometry",
-	        "stylers": [
-	            {
-	                "color": "#c5dac6"
-	            }
-	        ]
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c5dac6"
+          }
+        ]
 	    },
 	    {
-	        "featureType": "administrative",
-	        "stylers": [
-	            {
-	                "visibility": "on"
-	            },
-	            {
-	                "lightness": 33
-	            }
-	        ]
+        "featureType": "administrative",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "lightness": 33
+          }
+        ]
 	    },
 	    {
-	        "featureType": "road"
+        "featureType": "road"
 	    },
 	    {
-	        "featureType": "poi.park",
-	        "elementType": "labels",
-	        "stylers": [
-	            {
-	                "visibility": "on"
-	            },
-	            {
-	                "lightness": 20
-	            }
-	        ]
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "lightness": 20
+          }
+        ]
 	    },
 	    {},
 	    {
-	        "featureType": "road",
-	        "stylers": [
-	            {
-	                "lightness": 20
-	            }
-	        ]
+        "featureType": "road",
+        "stylers": [
+          {
+            "lightness": 20
+          }
+        ]
 	    }
 		]
 
 		var midpointId = $('#midpoint_id').text();
-		
+
 		$.get('/midpoints/' + midpointId +'/json_data', function(coordinates) {
 
 				mainMap = new GMaps ({
 					styles: styleArray,
 					div: '#main_map',
 					lat: coordinates.midpoint.lat,
-		      lng: coordinates.midpoint.lng
+		      lng: coordinates.midpoint.lng,
+
+		      mapTypeControl: false,
+		      streetViewControl: false,
+		      panControl: false,
+		      zoomControlOpt: { position: 'RIGHT_BOTTOM' }
 				});
 
 				mainMap.addMarker ({
 					lat: coordinates.midpoint.lat,
 		      lng: coordinates.midpoint.lng,
+		      icon: { url: '/assets/midpoint_pin.png' },
 		      id: 'midpoint'
 		    });
 
@@ -118,10 +124,19 @@ $(document).ready( function () {
 						lng: coordinates.address[index].lng,
 						id: 'address_' + (index + 1).toString()
 					});
+
+					mainMap.drawRoute({
+						origin: [coordinates.midpoint.lat, coordinates.midpoint.lng],
+						destination: [coordinates.address[index].lat, coordinates.address[index].lng],
+						travelMode: 'driving',
+					  strokeColor: 'royalblue',
+					  strokeOpacity: 0.6,
+					  strokeWeight: 3
+					});
 				});
 
 				mainMap.fitZoom();
-
+				mainMap.setCenter(coordinates.midpoint.lat, coordinates.midpoint.lng - 0.02);
 		});
 
 	};
