8d245bcaf5bb3038f85f14d89abce3793dac1f1f
cmew3 <c.wheeler-booth@cantab.net>
2014-09-07 18:28:27 +0100

    Fixed bug

--- app/assets/javascripts/venues.js
+++ app/assets/javascripts/venues.js
@@ -1,24 +1,26 @@
 $(document).ready( function () {
-  var venuesURL = document.URL + '/venues?options=topPicks'
-  
-  displayVenuesFrom(venuesURL);
-  
-  $('.find-venues').on('click', function(event) {
-    $('#venue-container').empty();
-    event.preventDefault();
-    displayVenuesFrom(this.href);
 
-  });
+  if( $('#venue-container').length ) {
+    var venuesURL = document.URL + '/venues?options=topPicks'
+    
+    displayVenuesFrom(venuesURL);
+    $('.find-venues').on('click', function(event) {
+      $('#venue-container').empty();
+      event.preventDefault();
+      displayVenuesFrom(this.href);
 
-  function displayVenuesFrom(url) {
-    $.get(url, function(data) {
-      data.venues.forEach( function(rawVenue) {
-        var venue = new VenueModel(rawVenue);
-        var venueCard = Mustache.render($('#venue-template').html(), venue);
-        $('#venue-container').append(venueCard);
-      });
     });
+
+    function displayVenuesFrom(url) {
+      $.get(url, function(data) {
+        data.venues.forEach( function(rawVenue) {
+          var venue = new VenueModel(rawVenue);
+          var venueCard = Mustache.render($('#venue-template').html(), venue);
+          $('#venue-container').append(venueCard);
+        });
+      });
+    };
+
   };
-  
 });
 
