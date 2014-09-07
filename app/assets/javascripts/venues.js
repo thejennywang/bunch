c7e06a1e0976318ee04664443fda032bb267a90a
cmew3 <c.wheeler-booth@cantab.net>
2014-09-07 16:36:40 +0100

    Updated venue model

--- app/assets/javascripts/venues.js
+++ app/assets/javascripts/venues.js
@@ -1,5 +1,11 @@
 $(document).ready( function () {
 
+  $.get(this.href, function(venues) {
+      venues.forEach( function(venue) {
+        var venueInfo = Mustache.render()
+      })
+    });
+
 	$('.find-venues').on('click', function(event) {
     event.preventDefault();
 
--- spec/jasmine/spec/VenueModelSpec.js
+++ spec/jasmine/spec/VenueModelSpec.js
@@ -1,45 +1,69 @@
 describe('VenueModel:', function() {
   
   var venue;
+  var venueData = {
+    "name": "The Charles Lamb",
+    "fullAddress": [
+    "16 Elia St (at Quick St)",
+    "Islington",
+    "Greater London",
+    "N1 8DE",
+    "United Kingdom"
+    ],
+    "lat": 51.5319382,
+    "lng": -0.1020542,
+    "category": "Wine Bar",
+    "priceTier": 2,
+    "rating": 8.9,
+    "id": "4ac518c5f964a520e2a420e3"
+  };
 
   beforeEach(function() {
-    venue = new VenueModel();
+    venue = new VenueModel(venueData);
   });
 
-  describe('defaults', function() {
+  describe('initialization', function() {
 
-    it('should have a default name of Unknown', function() {
-      expect(venue.name).toEqual('Unknown');
+    it('should have a name', function() {
+      expect(venue.name).toEqual('The Charles Lamb');
     });
   
-    it('should have a default address of Unknown', function() {
-      expect(venue.fullAddress).toEqual('Unknown');
+    it('should have an address', function() {
+      expect(venue.fullAddress).toContain('16 Elia St (at Quick St)');
     });
 
-    it('should have a default lat of 0.0', function() {
-      expect(venue.lat).toEqual(0.0);
+    it('should have a lat', function() {
+      expect(venue.lat).toEqual(51.5319382);
     });
 
-    it('should have a default lng of 0.0', function() {
-      expect(venue.lng).toEqual(0.0);
+    it('should have a lng', function() {
+      expect(venue.lng).toEqual(-0.1020542);
     });
 
-    it('should have a default price tier of N/A', function() {
-      expect(venue.priceTier).toEqual('N/A');
+    it('should have a priceTier', function() {
+      expect(venue.priceTier).toEqual(2);
     });
 
-    it('should have a default rating of N/A', function() {
-      expect(venue.rating).toEqual('N/A');
+    it('should have a rating', function() {
+      expect(venue.rating).toEqual(8.9);
     });
 
-    it('should have a default category of N/A', function() {
-      expect(venue.category).toEqual('N/A');
+    it('should have a category', function() {
+      expect(venue.category).toEqual('Wine Bar');
     });
 
-    it('should have a default id of N/A', function() {
-      expect(venue.id).toEqual('N/A');
+    it('should have an id', function() {
+      expect(venue.id).toEqual('4ac518c5f964a520e2a420e3');
     });
 
  });
+
+  describe('#formattedAddress', function() {
+
+    it('should return a formatted address', function() {
+      expect(venue.formattedAddress()).toEqual("16 Elia St (at Quick St), Islington, N1 8DE");
+    });
+
+  });
 
 });
