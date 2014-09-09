865da30d79ae1aacdb58dee4e16c1ed65f8b557c
Jeremy Fox <foxjerem@gmail.com>
2014-09-09 10:44:30 +0100

    Updated texting functionality

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,13 +1,17 @@
 class NotificationsController < ApplicationController
 
 	def new 
-
+		@meetup_url = params[:options]
 	end
 
 	def create
-		meetup_details = params[:message]
+		meetup_details = generate_meetup_details(params)
 		SMSNotifier.send_meetup_sms(params[:phone_numbers].first.values, meetup_details)
 		redirect_to new_midpoint_path
+	end
+
+	def generate_meetup_details(params)
+		"#{params[:message]}\n Venue details: #{params[:meetup_url]}\nPowered by ©bunch 2014" 
 	end
 
 end
