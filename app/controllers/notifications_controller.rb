d383c5cf8cbb7d09efbba6da966252486ad67c9e
Jeremy Fox <foxjerem@gmail.com>
2014-09-09 09:54:06 +0100

    tweaked notification controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,7 +5,6 @@ class NotificationsController < ApplicationController
 	end
 
 	def create
-		# raise 'hello'
 		meetup_details = params[:message]
 		SMSNotifier.send_meetup_sms(params[:phone_numbers].first.values, meetup_details)
 		redirect_to new_midpoint_path
