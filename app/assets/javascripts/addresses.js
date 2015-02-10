b2cec29d2bc4b0907c8cb8a8ff6df658795792d8
thejennywang <thejennywang@gmail.com>
2015-02-10 01:08:33 -0800

    Expanded area

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
   $('.carousel').carousel({interval: 5000});
 
   var maxAddresses = 5;
-  var southWest = new google.maps.LatLng(37.233158, -121.752533);
-  var northEast = new google.maps.LatLng(37.877659, -122.672638);
+  var southWest = new google.maps.LatLng(37.233158, -121.737512);
+  var northEast = new google.maps.LatLng(37.8011725, -122.146981);
   sfBounds = new google.maps.LatLngBounds(southWest, northEast);
 
   $('.bunch-submit').on('click', function(event) {
